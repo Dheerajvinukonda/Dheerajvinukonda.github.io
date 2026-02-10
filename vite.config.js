46ba755a01754a5e42722d0f6a6bc3940c1ac554
Dheeraj Vinukonda <vinukondadheeraj@gmail.com>
2026-02-11 03:17:02 +0530

    Build to docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ export default defineConfig({
     // This is critical for GitHub Pages project sites
     // It ensures assets use the correct subpath instead of root "/"
     base: '/Dheerajvinukonda.github.io/',
+    build: {
+        outDir: 'docs',
+    }
 });
